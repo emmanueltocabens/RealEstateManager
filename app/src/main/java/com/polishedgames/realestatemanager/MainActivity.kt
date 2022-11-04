7d51bf8ead11e1ff73ece95dbe5b4ee25321df3f
Emmanuel <tocab97@gmail.com>
2022-11-04 15:33:49 +0100

    commit initial

--- app/src/main/java/com/polishedgames/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/polishedgames/realestatemanager/MainActivity.kt
@@ -1,28 +1,35 @@
-package com.example.realestatemanager00
+package com.polishedgames.realestatemanager
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.realestatemanager00.ui.theme.RealEstateManager00Theme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.polishedgames.realestatemanager.ui.theme.RealEstateManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RealEstateManager00Theme {
+            RealEstateManagerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                ){}
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "login"){
+                    composable("home"){ HomeDestination()}
+                    composable("add"){ AddEstate()}
                 }
             }
         }
@@ -30,8 +37,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun InitNav(){
+fun HomeDestination(modifier: Modifier? = Modifier){
 
+}
+
+@Composable
+fun AddEstate(modifier: Modifier? = Modifier){
+
+}
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier){
+    val navController = rememberNavController()
+    Button(onClick = {navController.navigate("")}) {
+        Text(
+            text = "Login",
+            modifier = modifier
+        )
+    }
 }
 
 @Composable
@@ -44,8 +67,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RealEstateManager00Theme {
-        Greeting("Android")
+fun LoginPreview() {
+    RealEstateManagerTheme {
+        LoginScreen()
     }
 }
--- app/src/main/java/com/polishedgames/realestatemanager/ui/theme/Type.kt
+++ app/src/main/java/com/polishedgames/realestatemanager/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.realestatemanager00.ui.theme
+package com.polishedgames.realestatemanager.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
